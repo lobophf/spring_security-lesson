41d82451a9ca8a5a627ba64607572b9c5da2f414
Pedro Lobo <lobophf@gmail.com>
2022-04-06 16:46:30 -0300

    first commit

---
+++ src/test/java/com/springsecurity/trainning/TrainningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.trainning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
